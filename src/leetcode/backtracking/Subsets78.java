f51dc74f609a905155fbf98e32928658b74f7521
Bhanu Sai Pavan Kumar Pothuri <saipavanbhanu@gmail.com>
2022-07-14 14:36:02 +0530

    Solved using backtracking with O(N) space complexity

--- src/leetcode/backtracking/Subsets78.java
+++ src/leetcode/backtracking/Subsets78.java
@@ -3,34 +3,36 @@ package leetcode.backtracking;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Subsets78 {
-	class Solution {
-	    public List<List<Integer>> subsets(int[] nums) {
-	        int n = nums.length;
-	        int resultSize = (int)Math.pow(2,n);
-	        List<List<Integer>> result = new ArrayList<>(resultSize);
-	        
-	        for(int i = 0; i < resultSize; i++){
-	            result.add(getArray(i, nums));
-	        }
-	        return result;
-	    }
-	    private List<Integer> getArray(int no, int[]nums){
-	        List<Integer> arr = new ArrayList<>(nums.length);
-	        String binary = Integer.toBinaryString(no);
-	        int binaryIndex = binary.length()-1;
-	        for(int i = 0; i < nums.length; i++){
-	            int isSet = 0;
-	            if(binaryIndex >= 0){
-	                isSet = binary.charAt(binaryIndex)-'0';
-	            }else{
-	                isSet = 0;
-	            }
-	            if(isSet != 0 )
-	                arr.add(nums[i]*isSet);
-	            binaryIndex--;
-	        }
-	        return arr;
-	    }
-	}
-}
+public class Subsets78 {class Solution {
+    List<List<Integer>> result;
+    public List<List<Integer>> subsets(int[] nums) {
+        int n = nums.length;
+        int resultSize = (int)Math.pow(2,n);
+        result = new ArrayList<>(resultSize);
+        int binary[] = new int[nums.length];
+        
+        binary[0] = 0;
+        backtracking(1, nums, binary);
+        binary[0] = 1;
+        backtracking(1, nums, binary);
+        
+        return result;
+    }
+    
+    private void backtracking(int index, int[] nums, int[] binary){
+        if(index == nums.length){
+            List<Integer> list = new ArrayList<>(nums.length);
+            for(int i = 0; i < nums.length; i++){
+                if(binary[i] == 1){
+                    list.add(nums[i]);
+                }
+            }
+            result.add(list);
+        }else{
+            binary[index]=0;
+            backtracking(index+1, nums, binary);
+            binary[index]=1;
+            backtracking(index+1, nums, binary);
+        }
+    }
+}}
