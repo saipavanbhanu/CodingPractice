02ef431071e3b13a8b05fdd3c30a0cd3f05f8ff7
Bhanu Sai Pavan Kumar Pothuri <saipavanbhanu@gmail.com>
2022-07-21 17:15:32 +0530

    Update RangeSumOfBST938.java
    Skipping subtrees since it is BST.
    Faster solution and linear time complexity.

--- src/leetcode/tree/bst/RangeSumOfBST938.java
+++ src/leetcode/tree/bst/RangeSumOfBST938.java
@@ -2,7 +2,7 @@ package leetcode.tree.bst;
 
 public class RangeSumOfBST938 {
 
-	public class TreeNode {
+	class TreeNode {
 		int val;
 		TreeNode left;
 		TreeNode right;
@@ -25,13 +25,17 @@ public class RangeSumOfBST938 {
 		public int rangeSumBST(TreeNode root, int low, int high) {
 			if (root == null)
 				return 0;
-
-			if (root.val >= low && root.val <= high) {
-				return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
-			} else {
-				return rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+			int sum = 0;
+			if (root.val >= low && root.val <= high)
+				sum += root.val;
+			if (root.val > low) {
+				sum += rangeSumBST(root.left, low, high);
+			}
+			if (root.val < high) {
+				sum += rangeSumBST(root.right, low, high);
 			}
 
+			return sum;
 		}
 	}
 }
