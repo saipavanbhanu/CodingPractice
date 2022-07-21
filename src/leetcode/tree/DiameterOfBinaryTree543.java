9f67fbb60ae6f24456450594b3ed025a82de5be7
Bhanu Sai Pavan Kumar Pothuri <saipavanbhanu@gmail.com>
2022-07-21 15:04:59 +0530

    Update DiameterOfBinaryTree543.java
    Faster than 100% of the solutions.

--- src/leetcode/tree/DiameterOfBinaryTree543.java
+++ src/leetcode/tree/DiameterOfBinaryTree543.java
@@ -4,41 +4,46 @@ package leetcode.tree;
 max - variable to track the max diameter so far. and finally returning the max;*/
 
 public class DiameterOfBinaryTree543 {
-	  static class TreeNode {
-	      int val;
-	      TreeNode left;
-	      TreeNode right;
-	      TreeNode() {}
-	      TreeNode(int val) { this.val = val; }
-	      TreeNode(int val, TreeNode left, TreeNode right) {
-	          this.val = val;
-	          this.left = left;
-	          this.right = right;
-	      }
-	  }
-	 
+	static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
 	class Solution {
-	    int max;
-	    public int diameterOfBinaryTree(TreeNode root) {
-	        max = 0;
-	        dfs(root);
-	        return max;
-	    }
-	    
-	    //post order traversal //bottom up approach
-	    int dfs(TreeNode node){
-	        if(node == null)
-	            return 0;
-	        
-	        int lh = dfs(node.left);
-	        int rh = dfs(node.right);
-	        
-	        int withNode = lh + rh; //diameter which includes the node and left subtree and right subtree
-	        if(max < withNode){
-	            max = withNode;
-	        }
-	        
-	        //return the height
-	        return Math.max(lh, rh) + 1;
-	    }
-	}}
+		int max;
+
+		public int diameterOfBinaryTree(TreeNode root) {
+			max = 0;
+			dfs(root);
+			return max;
+		}
+
+		// post order traversal //bottom up approach
+		int dfs(TreeNode node) {
+			if (node == null)
+				return 0;
+
+			int lh = dfs(node.left);
+			int rh = dfs(node.right);
+
+			max = Math.max(max, lh + rh);
+
+			// return the height
+			return Math.max(lh, rh) + 1;
+		}
+	}
+}
