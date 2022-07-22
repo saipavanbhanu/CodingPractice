23df60fbc1f8142c5e566b19920d80028c645100
Bhanu Sai Pavan Kumar Pothuri <saipavanbhanu@gmail.com>
2022-07-22 10:58:34 +0530

    Update MergeTwoBinaryTrees617.java
    In place merging the binary trees.
    Memory efficient.

--- src/leetcode/tree/bt/MergeTwoBinaryTrees617.java
+++ src/leetcode/tree/bt/MergeTwoBinaryTrees617.java
@@ -23,19 +23,19 @@ public class MergeTwoBinaryTrees617 {
 	}
 
 	class Solution {
-		public TreeNode mergeTrees(TreeNode node1, TreeNode node2) {
-			if (node1 != null && node2 != null) {
-				TreeNode node = new TreeNode(node1.val + node2.val);
-				node.left = mergeTrees(node1.left, node2.left);
-				node.right = mergeTrees(node1.right, node2.right);
-				return node;
-			} else if (node1 == null) {
-				return node2;
-			} else if (node2 == null) {
-				return node1;
-			} else {
-				return null;
-			}
-		}
+	    public TreeNode mergeTrees(TreeNode node1, TreeNode node2) {
+	        if(node1 != null && node2 != null){
+	            node1.val = node1.val + node2.val;
+	            node1.left = mergeTrees(node1.left, node2.left);
+	            node1.right = mergeTrees(node1.right, node2.right);
+	            return node1;
+	        }else if(node1 == null){
+	            return node2;
+	        }else if(node2 == null){
+	            return node1;
+	        }else{
+	            return null;
+	        }
+	    }
 	}
 }
