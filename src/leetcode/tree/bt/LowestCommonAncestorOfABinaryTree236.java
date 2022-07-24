f5e434bf42d7f7e5afa8373df844e8ae0aab3825
Bhanu Sai Pavan Kumar Pothuri <saipavanbhanu@gmail.com>
2022-07-24 09:05:04 +0530

    Create LowestCommonAncestorOfABinaryTree236.java

---
+++ src/leetcode/tree/bt/LowestCommonAncestorOfABinaryTree236.java
@@ -0,0 +1,40 @@
+package leetcode.tree.bt;
+
+public class LowestCommonAncestorOfABinaryTree236 {
+
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+	class Solution {
+		public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+			if (root == null) {
+				return null;
+			} else {
+				if (root.val == p.val || root.val == q.val) {
+					return root;
+				} else {
+					TreeNode left = lowestCommonAncestor(root.left, p, q);
+					TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+					if (left != null && right != null) {
+						return root;
+					} else if (left != null) {
+						return left;
+					} else {
+						return right;
+					}
+
+				}
+
+			}
+
+		}
+	}
+}
