c65a2159fa8bc71ff8c61565839c51be90a67239
Bhanu Sai Pavan Kumar Pothuri <saipavanbhanu@gmail.com>
2022-07-26 12:47:33 +0530

    Update PopulatingNextRightPointersInEachNode116.java
    faster than 50% of solutions.

--- src/leetcode/tree/PopulatingNextRightPointersInEachNode116.java
+++ src/leetcode/tree/PopulatingNextRightPointersInEachNode116.java
@@ -1,7 +1,7 @@
 package leetcode.tree;
 
-import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.LinkedList;
 
 public class PopulatingNextRightPointersInEachNode116 {
 
@@ -28,37 +28,37 @@ public class PopulatingNextRightPointersInEachNode116 {
 	};
 
 	class Solution {
-		public Node connect(Node root) {
-			if (root == null) {
-				return root;
-			}
-
-			Deque<Node> q = new ArrayDeque<>();
-			q.add(root);
-
-			while (!q.isEmpty()) {
-				int currSize = q.size();
-				while (currSize - 1 > 0) {
-					Node node = q.remove();
-					if (node.left != null) {
-						q.add(node.left);
-					}
-					if (node.right != null) {
-						q.add(node.right);
-					}
-					node.next = q.getFirst();
-					currSize--;
-				}
-				Node node = q.remove();
-				node.next = null;
-				if (node.left != null) {
-					q.add(node.left);
-				}
-				if (node.right != null) {
-					q.add(node.right);
-				}
-			}
-			return root;
-		}
+	    public Node connect(Node root) {
+	        if(root == null){
+	            return root;
+	        }
+	        
+	        Deque<Node> q = new LinkedList<>();
+	        q.addLast(root);
+	        
+	        while(!q.isEmpty()){
+	            int currSize = q.size();
+	            while(currSize-1 > 0){
+	                Node node = q.removeFirst();
+	                if(node.left != null){
+	                    q.addLast(node.left);
+	                }
+	                if(node.right != null){
+	                    q.addLast(node.right);
+	                }
+	                node.next = q.getFirst();
+	                currSize--;
+	            }
+	            Node node = q.remove();
+	            node.next = null;
+	            if(node.left != null){
+	                q.addLast(node.left);
+	            }
+	            if(node.right != null){
+	                q.addLast(node.right);
+	            }
+	        }
+	        return root;
+	    }
 	}
 }
