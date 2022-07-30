3822d2e953f7a1d348a1ed2fe2cfbeea6b4957c2
Bhanu Sai Pavan Kumar Pothuri <saipavanbhanu@gmail.com>
2022-07-30 09:32:23 +0530

    Update KthLargestElementInAnArray215.java
    Using quick select algorithm.

--- src/leetcode/heap/KthLargestElementInAnArray215.java
+++ src/leetcode/heap/KthLargestElementInAnArray215.java
@@ -1,31 +1,64 @@
 package leetcode.heap;
 
-import java.util.PriorityQueue;
-
 public class KthLargestElementInAnArray215 {
 	class Solution {
-		public int findKthLargest(int[] nums, int k) {
-
-			PriorityQueue<Integer> pq = new PriorityQueue<>(k);
-
-			// maintain a heap of size of k ( k - largest elements)
-			for (int i = 0; i < nums.length; i++) {
-				if (pq.size() < k) {
-					pq.add(nums[i]);
-				} else {
-					pq.add(nums[i]);
-					pq.poll();
-				}
-			}
-
-			// int size = pq.size();
-			// while(size-1 > 0){
-			// pq.poll();
-			// size--;
-			// }
-			int result = pq.poll();
-			pq = null;
-			return result;
-		}
+	    public int findKthLargest(int[] nums, int k) {
+	        
+	        int pivot = -1;
+	        int start = 0, end = nums.length-1;
+	        do{
+	            //System.out.println("start:"+start+",end:"+end);
+	            pivot = partition(nums, start, end);
+	            //System.out.println("pivot-value:"+nums[pivot]+"; index:"+pivot);
+	            //System.out.println(Arrays.toString(nums));
+	            if(pivot+1 == k){
+	                return nums[pivot];
+	            }else if(pivot+1 < k){
+	                start = pivot + 1;
+	            }else{
+	                end = pivot - 1; 
+	            }
+	        }while(true);
+	        
+	    }
+	    
+	    int partition(int[] nums, int left, int right){
+	        int i = left;
+	        int pivot = right;
+	        int j = right-1;
+	        if(i == j){
+	            if(nums[pivot] < nums[i]){
+	                //do nothing;
+	                return pivot;
+	            }else{
+	                //swap the pivot and i
+	                int temp = nums[pivot];
+	                nums[pivot] = nums[i];
+	                nums[i] = temp;
+	                return pivot;
+	            }
+	        }else{
+	            //System.out.println("i:"+i+",j:"+j);
+	            while(i < j){
+	                while(i < j && nums[i] > nums[pivot]){
+	                    i++;
+	                }
+	                while(j > 0 && j > i && nums[j] <= nums[pivot]){
+	                    j--;
+	                }
+	                if(i < j){
+	                    int temp = nums[i];
+	                    nums[i] = nums[j];
+	                    nums[j] = temp;
+	                }
+	            }
+	            //System.out.println("i:"+i+",j:"+j+",pivot-value:"+nums[pivot]);
+	            
+	            int temp = nums[i];
+	            nums[i] = nums[pivot];
+	            nums[pivot] = temp;            
+	            return i;    
+	        }
+	    }
 	}
 }
