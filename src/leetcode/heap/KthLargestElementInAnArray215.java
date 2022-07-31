ab06b6c2a74eec7f93f03afb6460743636a02461
Bhanu Sai Pavan Kumar Pothuri <saipavanbhanu@gmail.com>
2022-07-31 09:05:50 +0530

    Update KthLargestElementInAnArray215.java
    Working quick-select solution.

--- src/leetcode/heap/KthLargestElementInAnArray215.java
+++ src/leetcode/heap/KthLargestElementInAnArray215.java
@@ -2,63 +2,37 @@ package leetcode.heap;
 
 public class KthLargestElementInAnArray215 {
 	class Solution {
-	    public int findKthLargest(int[] nums, int k) {
-	        
-	        int pivot = -1;
-	        int start = 0, end = nums.length-1;
-	        do{
-	            //System.out.println("start:"+start+",end:"+end);
-	            pivot = partition(nums, start, end);
-	            //System.out.println("pivot-value:"+nums[pivot]+"; index:"+pivot);
-	            //System.out.println(Arrays.toString(nums));
-	            if(pivot+1 == k){
-	                return nums[pivot];
-	            }else if(pivot+1 < k){
-	                start = pivot + 1;
-	            }else{
-	                end = pivot - 1; 
-	            }
-	        }while(true);
-	        
-	    }
-	    
-	    int partition(int[] nums, int left, int right){
-	        int i = left;
-	        int pivot = right;
-	        int j = right-1;
-	        if(i == j){
-	            if(nums[pivot] < nums[i]){
-	                //do nothing;
-	                return pivot;
-	            }else{
-	                //swap the pivot and i
-	                int temp = nums[pivot];
-	                nums[pivot] = nums[i];
-	                nums[i] = temp;
-	                return pivot;
-	            }
-	        }else{
-	            //System.out.println("i:"+i+",j:"+j);
-	            while(i < j){
-	                while(i < j && nums[i] > nums[pivot]){
-	                    i++;
-	                }
-	                while(j > 0 && j > i && nums[j] <= nums[pivot]){
-	                    j--;
-	                }
-	                if(i < j){
-	                    int temp = nums[i];
-	                    nums[i] = nums[j];
-	                    nums[j] = temp;
-	                }
-	            }
-	            //System.out.println("i:"+i+",j:"+j+",pivot-value:"+nums[pivot]);
-	            
-	            int temp = nums[i];
-	            nums[i] = nums[pivot];
-	            nums[pivot] = temp;            
-	            return i;    
-	        }
-	    }
+		public int findKthLargest(int[] nums, int k) {
+			return quickSelect(nums, 0, nums.length - 1, nums.length - k);
+		}
+
+		int quickSelect(int[] nums, int l, int r, int k) {
+			int pivot = nums[r];
+			int p = l;
+			int i = -1;
+			for (i = l; i < r; i++) {
+				if (nums[i] <= pivot) {
+					swap(nums, p, i);
+					p++;
+				}
+			}
+			swap(nums, p, r);
+			if (p < k) {
+				return quickSelect(nums, p + 1, r, k);
+			} else if (p > k) {
+				return quickSelect(nums, l, p - 1, k);
+			} else {
+				return nums[p];
+			}
+		}
+
+		void swap(int[] nums, int i, int j) {
+			if (i != j) {
+				int temp = nums[i];
+				nums[i] = nums[j];
+				nums[j] = temp;
+			}
+		}
+
 	}
 }
