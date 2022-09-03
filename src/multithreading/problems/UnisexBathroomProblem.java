136cc992bae16fb64090ccb68d4fe6cfdba63487
Bhanu Sai Pavan Kumar Pothuri <saipavanbhanu@gmail.com>
2022-09-03 16:08:42 +0530

    Create UnisexBathroomProblem.java
    Some Deadlock happening. Need to resolve the issue.

---
+++ src/multithreading/problems/UnisexBathroomProblem.java
@@ -0,0 +1,108 @@
+package multithreading.problems;
+
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
+public class UnisexBathroomProblem {
+
+	public static void main(String[] args) {
+		UnisexBathRoom obj = new UnisexBathRoom(3);
+		
+		for(int i = 0; i < 10; i++) {
+			Thread t = new Thread() {
+				@Override
+				public void run() {
+					Random r = new Random();
+					int i = r.nextInt();
+					if(i % 2 == 0) {
+						try {
+							obj.useBathroomByMale(Sex.MALE, String.valueOf(i));
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}else {
+						try {
+							obj.useBathroomByFemale(Sex.FEMALE, String.valueOf(i));
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+			};
+			t.start();
+		}
+	}
+
+}
+
+class UnisexBathRoom {
+	Sex inUseBy;
+	Semaphore capacity;
+	Semaphore lock;
+	int size; // no of persons already present in the bathroom
+
+	public UnisexBathRoom(int capacity) {
+		inUseBy = Sex.NONE;
+		this.capacity = new Semaphore(capacity);
+		size = 0;
+		lock = new Semaphore(1);
+	}
+
+	public void useBathroomByMale(Sex user, String name) throws InterruptedException {
+
+		synchronized (this) {
+			if (inUseBy != user && inUseBy != Sex.NONE) {
+				wait();
+			}
+			System.out.println("Male:"+name + " trying to enter bathroom");
+			capacity.acquire();
+			inUseBy = user;
+			System.out.println("Male:"+name + " entered the bathroom");
+			size++;
+		}
+		System.out.println("Male:"+name + " using the bathroom");
+		Thread.sleep(1000);
+
+		synchronized (this) {
+			capacity.release();
+			size--;
+			if (size == 0)
+				inUseBy = Sex.NONE;
+			notifyAll();
+		}
+
+		System.out.println("Male:"+name + " leaving the bathroom");
+	}
+	
+	public void useBathroomByFemale(Sex user, String name) throws InterruptedException {
+		
+		synchronized (this) {
+			if (inUseBy != user && inUseBy != Sex.NONE) {
+				wait();
+			}
+			System.out.println("Female:"+name + " trying to enter bathroom");
+			capacity.acquire();
+			inUseBy = user;
+			System.out.println("Female:"+name + " entered the bathroom");
+			size++;
+		}
+		System.out.println("Female:"+name + " using the bathroom");
+		Thread.sleep(500);
+
+		synchronized (this) {
+			capacity.release();
+			size--;
+			if (size == 0)
+				inUseBy = Sex.NONE;
+			notifyAll();
+		}
+
+		System.out.println("Female:"+name + " leaving the bathroom");
+	}
+}
+
+enum Sex {
+	MALE, FEMALE, NONE;
+}
