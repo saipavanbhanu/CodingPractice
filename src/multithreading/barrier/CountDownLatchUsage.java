fb6158fca047634be17cf5005e3b11c57aa702a9
Bhanu Sai Pavan Kumar Pothuri <saipavanbhanu@gmail.com>
2025-07-09 13:21:27 +0530

    Count down latch initialized with 3 and awaits until 3 have reached.

---
+++ src/multithreading/barrier/CountDownLatchUsage.java
@@ -0,0 +1,46 @@
+package multithreading.barrier;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+public class CountDownLatchUsage {
+    public static void main(String[] args) {
+        CountDownLatch latch = new CountDownLatch(3);
+        List<Thread> list = new ArrayList<>();
+        for(int i = 0; i < 3; i++){
+            Thread t = new Thread(new Worker(latch, Integer.toString(i)));
+            list.add(t);
+            t.start();
+        }
+        for(Thread t : list){
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println("Main thread after count down latch finished.");
+    }
+}
+
+class Worker implements Runnable{
+    CountDownLatch latch;
+    String name;
+    Worker(CountDownLatch latch, String name){
+        this.latch = latch;
+        this.name = name;
+    }
+    @Override
+    public void run() {
+        System.out.println("Thread "+name+": init");
+        latch.countDown();
+        try {
+            System.out.println("Thread "+name+" before wait");
+            latch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Thread "+name+" after thread await finished.");
+    }
+}
